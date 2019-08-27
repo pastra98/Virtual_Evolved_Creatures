70deb6f2c2e4e9d33acca0414b09a05b316ccab9
Paul Straberger <p.straberger@gmail.com>
2019-08-27 19:16:30 +0200

    added bones class

--- creatures.py
+++ creatures.py
@@ -1,10 +1,7 @@
+from geom_creation import Geometry_op
 from panda3d import core as cor
 from panda3d.core import Geom as geo
 from direct.showbase import ShowBase as shb
-from geom_creation import Geometry_op
-
-# init ShowBase class in order to get a scene graph and other stuff
-base = shb.ShowBase()
 
 class Creature(Geometry_op):
     """
@@ -16,7 +13,26 @@ class Creature(Geometry_op):
         and builds a first structure.
         """
         self.skelleton = []
-        self.skelleton.append(self.build_structure("bone1"))
+        # self.skelleton.append(self.build_structure("bone1"))
 
-my_creature = Creature()
-print(my_creature.skelleton)
+
+class Bone(Geometry_op):
+    """
+    this class inherits from Geometry_op, and provides certain
+    operations, that can be of use when creating the morphology of a
+    creature. It also holds a collection of shapes, that can later be
+    called upon
+    """
+    def __init__(self, name):
+        """
+        For now this just creates a default bone, all this will be
+        changed later.
+        """
+        # initialize parent Geometry_op with given name
+        super().__init__(name)
+
+        self.base_tri = self.make_tri([0, 100, 0], [10, 100, 0],
+                                                   [0, 100, 10])
+
+
+
