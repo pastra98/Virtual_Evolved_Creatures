7dc2cdb3a0df3b75ef879cdfc41def8a13016e27
Paul Straberger <p.straberger@gmail.com>
2019-08-27 19:15:28 +0200

    this is just a test, but this will be the main file executing showbase etc

---
+++ mainfile_test.py
@@ -0,0 +1,23 @@
+from geom_creation import Geometry_op
+from creatures import Creature
+from creatures import Bone
+from panda3d import core as cor
+from panda3d.core import Geom as geo
+from direct.showbase import ShowBase as shb
+
+# init ShowBase class in order to get a scene graph and other stuff
+base = shb.ShowBase()
+
+# create a geometry_op object
+my_bone = Bone("bonez")
+
+# creating a nodepath
+Node = cor.GeomNode("cube2")
+
+# adding our triangle to the newly created node
+Node.addGeom(my_bone.b_geom)
+
+# creating a nodepath for our cube and attaching it to render
+NodePath = render.attachNewNode(Node)
+
+base.run()
