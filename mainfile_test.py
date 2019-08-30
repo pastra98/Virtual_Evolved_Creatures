f44d019fecf2e2cfd893d05faeac6875d6a5617e
Paul Straberger <p.straberger@gmail.com>
2019-08-30 20:34:54 +0200

    attached light source to camera

--- mainfile_test.py
+++ mainfile_test.py
@@ -2,8 +2,8 @@ from tetrahedrical import tetrahedrical
 from geom_creation import Geometry_op
 from creatures import Creature
 from creatures import Bone
-from panda3d import core as cor
 
+from panda3d import core as cor
 from panda3d.core import Geom as geo
 from direct.showbase import ShowBase as shb
 
@@ -12,8 +12,13 @@ base = shb.ShowBase()
 
 # create a geometry_op object
 my_bone = Bone("bonez")
-print(my_bone.f_list)
-print(my_bone.b_geom)
+
+# setting up a light source
+p_light = cor.PointLight("point_light")
+p_light.set_color((1., 1., 1., 1.))
+light = camera.attach_new_node(p_light)
+light.set_pos(5., -10., 7.)
+render.set_light(light)
 
 # creating a nodepath
 Node = cor.GeomNode("cube2")
