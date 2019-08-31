3445897c5e2a874fa9bb7162d72a141a2565137d
Paul Straberger <p.straberger@gmail.com>
2019-08-31 18:24:37 +0200

    finally moved Bone class to a seperate file

--- creatures.py
+++ creatures.py
@@ -2,8 +2,6 @@ from tetrahedrical import tetrahedrical
 from geom_creation import Geometry_op
 
 from panda3d import core as cor
-from panda3d.core import Geom as geo
-from direct.showbase import ShowBase as shb
 
 class Creature():
     """
@@ -16,29 +14,4 @@ class Creature():
         """
         self.skelleton = []
         # self.skelleton.append(self.build_structure("bone1"))
-
-
-class Bone(tetrahedrical):
-    """
-    this class inherits from Geometry_op, and provides certain
-    operations, that can be of use when creating the morphology of a
-    creature. It also holds a collection of shapes, that can later be
-    called upon
-    """
-    def __init__(self, name):
-        """
-        For now this just creates a default bone, all this will be
-        changed later.
-        """
-        super().__init__(name)
-        base_triangle = ["0", cor.LPoint3f(0, 0, 0),
-                              cor.LPoint3f(10, 0, 0),
-                              cor.LPoint3f(0, 10, 0)]
-
-        build_instructions = [[1, "0", [0.3, 0.3], 5],
-                              [2, "1A", [0.3, 0.3], 5],
-                              [3, "1B", [0.3, 0.3], 5]]
-
-        self.build_struct(base_triangle, build_instructions)
-
 
